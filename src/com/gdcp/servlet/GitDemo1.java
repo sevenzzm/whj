529b04f42355a51860e44fa244c5ef1da5f8d761
sevenzzm <982174832@qq.com>
2019-04-03 20:34:58 +0800

    添加GitDemo1类

---
+++ src/com/gdcp/servlet/GitDemo1.java
@@ -0,0 +1,15 @@
+package com.gdcp.servlet;
+
+/**
+ * @ClassName GitDemo1
+ * @Description
+ * @Author Sevens
+ * @Date 20:33 2019/4/3
+ * @Version 2.1
+ **/
+
+public class GitDemo1 {
+    public static void main(String[] args){
+        System.out.println(1);
+    }
+}
